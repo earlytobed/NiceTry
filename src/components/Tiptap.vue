d1df5b91828face6e2b7213b6d727a1c459de43d
Luke Yue <lukedyue@gmail.com>
2021-11-12 11:27:25 +0800

    feat(Tiptap): Add Image insert button style(Tiptap): Refine some elements
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- src/components/Tiptap.vue
+++ src/components/Tiptap.vue
@@ -2,7 +2,7 @@
   <v-card class="mx-2">
     <div v-if="editor">
       <div class="editor-toolbar">
-        <v-toolbar>
+        <v-toolbar class="hidden-sm-and-down" flat color="grey lighten-4">
           <v-menu offset-y>
             <template v-slot:activator="{ on, attrs }">
               <v-btn
@@ -57,57 +57,76 @@
             small
             icon
             @click="editor.chain().focus().toggleBold().run()"
-            :class="{ 'v-btn--active': editor.isActive('bold') }"
+            :class="{
+              'v-btn--active': editor.isActive('bold'),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-format-bold</v-icon>
+            <v-icon size="16">fa-bold</v-icon>
           </v-btn>
           <v-btn
             small
             icon
             @click="editor.chain().focus().toggleItalic().run()"
-            :class="{ 'v-btn--active': editor.isActive('italic') }"
+            :class="{
+              'v-btn--active': editor.isActive('italic'),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-format-italic</v-icon>
+            <v-icon size="16">fa-italic</v-icon>
           </v-btn>
           <v-btn
             small
             icon
             @click="editor.chain().focus().toggleStrike().run()"
-            :class="{ 'v-btn--active': editor.isActive('strike') }"
+            :class="{
+              'v-btn--active': editor.isActive('strike'),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-format-strikethrough</v-icon>
+            <v-icon size="16">fa-strikethrough</v-icon>
           </v-btn>
           <v-btn
             small
             icon
             @click="editor.chain().focus().toggleUnderline().run()"
-            :class="{ 'v-btn--active': editor.isActive('underline') }"
+            :class="{
+              'v-btn--active': editor.isActive('underline'),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-format-underline</v-icon>
+            <v-icon size="16">fa-underline</v-icon>
           </v-btn>
           <v-btn
             small
             icon
             @click="editor.chain().focus().toggleCode().run()"
-            :class="{ 'v-btn--active': editor.isActive('code') }"
+            :class="{
+              'v-btn--active': editor.isActive('code'),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-code-tags</v-icon>
+            <v-icon size="16">fa-code</v-icon>
           </v-btn>
           <v-btn
             small
             icon
             @click="editor.chain().focus().unsetAllMarks().run()"
+            class="tiptap-editor-btn"
           >
-            <v-icon>mdi-close-circle-outline</v-icon>
+            <v-icon size="16">fa-ban</v-icon>
           </v-btn>
           <v-divider vertical class="mx-1"></v-divider>
           <v-btn
             small
             icon
             @click="editor.chain().focus().setTextAlign('left').run()"
-            :class="{ 'v-btn--active': editor.isActive({ textAlign: 'left' }) }"
+            :class="{
+              'v-btn--active': editor.isActive({ textAlign: 'left' }),
+              'tiptap-editor-btn': true,
+            }"
           >
-            <v-icon>mdi-format-align-left</v-icon>
+            <v-icon size="16">fa-align-left</v-icon>
           </v-btn>
           <v-btn
             small
@@ -115,9 +134,10 @@
             @click="editor.chain().focus().setTextAlign('center').run()"
             :class="{
               'v-btn--active': editor.isActive({ textAlign: 'center' }),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-align-center</v-icon>
+            <v-icon size="16">fa-align-center</v-icon>
           </v-btn>
           <v-btn
             small
@@ -125,9 +145,10 @@
             @click="editor.chain().focus().setTextAlign('right').run()"
             :class="{
               'v-btn--active': editor.isActive({ textAlign: 'right' }),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-align-right</v-icon>
+            <v-icon size="16">fa-align-right</v-icon>
           </v-btn>
           <v-divider vertical class="mx-1"></v-divider>
           <v-btn
@@ -136,49 +157,77 @@
             @click="editor.chain().focus().setParagraph().run()"
             :class="{
               'v-btn--active': editor.isActive('paragraph'),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-paragraph</v-icon>
+            <v-icon size="16">fa-paragraph</v-icon>
           </v-btn>
-          <v-btn
-            small
-            icon
-            @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-            :class="{
-              'v-btn--active': editor.isActive('heading', { level: 1 }),
-            }"
-          >
-            <v-icon>mdi-format-header-1</v-icon>
-          </v-btn>
-          <v-btn
-            small
-            icon
-            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-            :class="{
-              'v-btn--active': editor.isActive('heading', { level: 2 }),
-            }"
-          >
-            <v-icon>mdi-format-header-2</v-icon>
-          </v-btn>
-          <v-btn
-            small
-            icon
-            @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-            :class="{
-              'v-btn--active': editor.isActive('heading', { level: 3 }),
-            }"
-          >
-            <v-icon>mdi-format-header-3</v-icon>
-          </v-btn>
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                small
+                icon
+                :class="{
+                  'v-btn--active': editor.isActive('heading'),
+                  'tiptap-editor-btn': true,
+                }"
+              >
+                <v-icon size="16">fa-heading</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="editor.chain().focus().setParagraph().run()">
+                <span>Paragraph</span>
+              </v-list-item>
+              <v-list-item
+                @click="
+                  editor.chain().focus().toggleHeading({ level: 1 }).run()
+                "
+              >
+                <h1>Heading 1</h1>
+              </v-list-item>
+              <v-list-item
+                @click="
+                  editor.chain().focus().toggleHeading({ level: 2 }).run()
+                "
+              >
+                <h2>Heading 2</h2>
+              </v-list-item>
+              <v-list-item
+                @click="
+                  editor.chain().focus().toggleHeading({ level: 3 }).run()
+                "
+              >
+                <h3>Heading 3</h3>
+              </v-list-item>
+              <v-list-item
+                @click="
+                  editor.chain().focus().toggleHeading({ level: 4 }).run()
+                "
+              >
+                <h4>Heading 4</h4>
+              </v-list-item>
+              <v-list-item
+                @click="
+                  editor.chain().focus().toggleHeading({ level: 5 }).run()
+                "
+              >
+                <h5>Heading 5</h5>
+              </v-list-item>
+            </v-list>
+          </v-menu>
           <v-btn
             small
             icon
             @click="editor.chain().focus().toggleBulletList().run()"
             :class="{
               'v-btn--active': editor.isActive('bulletList'),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-list-bulleted</v-icon>
+            <v-icon size="16">fa-list-ul</v-icon>
           </v-btn>
           <v-btn
             small
@@ -186,9 +235,10 @@
             @click="editor.chain().focus().toggleOrderedList().run()"
             :class="{
               'v-btn--active': editor.isActive('orderedList'),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-list-numbered</v-icon>
+            <v-icon size="16">fa-list-ol</v-icon>
           </v-btn>
           <v-btn
             small
@@ -196,9 +246,10 @@
             @click="editor.chain().focus().toggleCodeBlock().run()"
             :class="{
               'v-btn--active': editor.isActive('codeBlock'),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-code-braces</v-icon>
+            <v-icon size="16">bi-code-square</v-icon>
           </v-btn>
           <v-btn
             small
@@ -206,17 +257,98 @@
             @click="editor.chain().focus().toggleBlockquote().run()"
             :class="{
               'v-btn--active': editor.isActive('blockquote'),
+              'tiptap-editor-btn': true,
             }"
           >
-            <v-icon>mdi-format-quote-close</v-icon>
+            <v-icon size="16">fa-quote-right</v-icon>
+          </v-btn>
+          <v-btn
+            small
+            icon
+            @click="editor.chain().focus().setHorizontalRule().run()"
+            class="tiptap-editor-btn"
+          >
+            <v-icon size="16">fa-minus</v-icon>
           </v-btn>
           <v-divider vertical class="mx-1"></v-divider>
-          <v-btn small icon @click="editor.chain().focus().undo().run()">
-            <v-icon>mdi-undo</v-icon>
+          <v-btn
+            small
+            icon
+            @click="editor.chain().focus().undo().run()"
+            class="tiptap-editor-btn"
+          >
+            <v-icon size="16">fa-undo</v-icon>
           </v-btn>
-          <v-btn small icon @click="editor.chain().focus().redo().run()">
-            <v-icon>mdi-redo</v-icon>
+          <v-btn
+            small
+            icon
+            @click="editor.chain().focus().redo().run()"
+            class="tiptap-editor-btn"
+          >
+            <v-icon size="16">fa-redo</v-icon>
           </v-btn>
+          <v-divider vertical class="mx-1"></v-divider>
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+                small
+                icon
+                class="tiptap-editor-btn"
+              >
+                <v-icon size="16">fa-image</v-icon>
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">Add Image</span>
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Link to image"
+                        required
+                        v-model="imageUrl"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12">
+                      <v-text-field
+                        label="Alternative text (optional)"
+                        v-model="imageText"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="
+                    dialog = false;
+                    imageUrl = '';
+                    imageText = '';
+                  "
+                >
+                  Close
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="
+                    dialog = false;
+                    addImage();
+                  "
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
       </div>
     </div>
@@ -232,6 +364,8 @@ import TextAlign from "@tiptap/extension-text-align";
 import FontFamily from "@tiptap/extension-font-family";
 import TextStyle from "@tiptap/extension-text-style";
 import Color from "@tiptap/extension-color";
+import Placeholder from "@tiptap/extension-placeholder";
+import Image from "@tiptap/extension-image";
 
 export default {
   components: {
@@ -243,6 +377,9 @@ export default {
       editor: null,
       fontTypes: ["sans-serif", "serif", "monospace"],
       colorHex: "#000000",
+      dialog: false,
+      imageUrl: "",
+      imageText: "",
     };
   },
 
@@ -277,9 +414,25 @@ export default {
     },
   },
 
+  methods: {
+    addImage() {
+      if (this.imageUrl && this.imageText) {
+        this.editor
+          .chain()
+          .focus()
+          .setImage({ src: this.imageUrl, alt: this.imageText })
+          .run();
+        this.imageUrl = "";
+        this.imageText = "";
+      } else if (this.imageUrl) {
+        this.editor.chain().focus().setImage({ src: this.imageUrl }).run();
+        this.imageUrl = "";
+      }
+    },
+  },
+
   mounted() {
     this.editor = new Editor({
-      content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
       extensions: [
         StarterKit,
         Underline,
@@ -294,6 +447,13 @@ export default {
         }),
         Color.configure({
           types: ["textStyle"],
+        }),
+        Placeholder.configure({
+          emptyEditorClass: "is-editor-empty",
+          placeholder: "Write torrent descriptions here...",
+        }),
+        Image.configure({
+          inline: false,
         }),
       ],
     });
@@ -320,6 +480,32 @@ export default {
 
 .content {
   text-align: left;
-  margin: 4px 4px 4px 4px;
+  padding: 20px 20px;
+  min-height: 180px;
+}
+
+.ProseMirror p.is-editor-empty:first-child::before {
+  content: attr(data-placeholder);
+  float: left;
+  color: #adb5bd;
+  pointer-events: none;
+  height: 0;
+}
+
+.ProseMirror:focus {
+  outline: none;
+}
+
+.tiptap-editor-btn {
+  margin: 2px 4px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+
+  &.ProseMirror-selectednode {
+    outline: 3px solid #68cef8;
+  }
 }
 </style>
